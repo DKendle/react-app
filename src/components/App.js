f3510f8863c0d17cedde5c3122f5635195a1100a
DKendle <dina.kendle@gmail.com>
2021-12-03 23:43:52 -0500

    React-router-dom v 6, adds links and routes to about/home comps in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Home from "./Home";
 import About from "./About";
+import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
+
 
 function App() {
+
   return (
     <div className="App">
-      <Home />
-      <About />
+      <BrowserRouter>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/' element={<Home />} />
+
+        </Routes>
+      </BrowserRouter> 
     </div>
   );
 }
