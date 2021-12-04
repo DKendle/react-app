423ba4439c930a4e4cc1af38360ca12bcf5effff
DKendle <dina.kendle@gmail.com>
2021-12-03 23:23:27 -0500

    Adds about and home components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
